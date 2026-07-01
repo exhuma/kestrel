913b25e84b9db977fde430c977642a60a520b97c
agent-dispatcher <dispatcher@local>
2026-07-01 22:24:46 +0200

    Implement #1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,11 @@ import SessionPanel from './components/SessionPanel.vue'
 
 <template>
   <v-app>
-    <v-app-bar color="primary" title="agent-dispatcher" />
+    <v-app-bar color="primary" title="agent-dispatcher">
+      <template #prepend>
+        <img src="/logo.svg" alt="agent-dispatcher logo" height="40" class="ms-2" />
+      </template>
+    </v-app-bar>
     <v-main>
       <SessionPanel />
     </v-main>
