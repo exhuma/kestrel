e0a7bdb8ba8aea1d83af660be3b3e37b152619ab
kestrel <kestrel@local>
2026-07-02 09:38:54 +0200

    Implement #3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,9 @@ import SessionPanel from './components/SessionPanel.vue'
   <v-app>
     <v-app-bar color="primary" title="agent-dispatcher">
       <template #prepend>
-        <img src="/logo.svg" alt="agent-dispatcher logo" height="40" class="ms-2" />
+        <div class="d-flex align-center ms-2">
+          <img src="/logo.svg" alt="agent-dispatcher logo" height="40" />
+        </div>
       </template>
     </v-app-bar>
     <v-main>
